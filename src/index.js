65288e9475ea7f5b0c5a50ccfe00802ece462ecd
Jack Lee(Teng Hong Lee) <43887352+007jackying@users.noreply.github.com>
2021-01-11 10:50:44 -0330

    meta tag updated to resolve http

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes'; // where we are going to specify our routes
 import './App.css';
 import axios from 'axios';
+import MetaTags from 'react-meta-tags';
 
 axios.defaults.baseURL = 'http://134.122.40.2/';
 
 ReactDOM.render(
   <>
+    <MetaTags>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+    </MetaTags>
 
-  <Router>
-    <Routes />
-  </Router>
+    <Router>
+      <Routes />
+    </Router>
   </>,
   document.getElementById('root')
 );
